9d2d98a9fac1b6eb09a3a849a43976453ba40556
jvb2820 <140871447+jvb2820@users.noreply.github.com>
2026-01-30 16:10:07 +0800

    Update SignIn.tsx

--- components/SignIn.tsx
+++ components/SignIn.tsx
@@ -129,7 +129,7 @@ const SignIn: React.FC<SignInProps> = ({ onLogin }) => {
           </form>
         </div>
       </div>
-      <footer className="flex flex-col justify-center items-center bg-transparent mt-2">
+      <footer className="flex flex-col justify-center items-center bg-transparent mt-3">
         <div className="bg-white border border-border-color rounded-lg px-6 py-2 shadow-sm flex items-center justify-center mb-2">
           <img src={logo} alt="Logo" className="h-8 w-auto object-contain" />
         </div>
