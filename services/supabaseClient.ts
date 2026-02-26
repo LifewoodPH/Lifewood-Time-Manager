5d2e813d0208d19271a80a8ffade74083c425501
jvb2820 <140871447+jvb2820@users.noreply.github.com>
2026-02-26 10:36:30 +0800

    database migration

--- services/supabaseClient.ts
+++ services/supabaseClient.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from '../types';
 
-const supabaseUrl = 'https://szifmsvutxcrcwfjbvsi.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6aWZtc3Z1dHhjcmN3ZmpidnNpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAwMjcwNzEsImV4cCI6MjA3NTYwMzA3MX0.hvZKMI0NDQ8IdWaDonqmiyvQu-NkCN0nRHPjn0isoCA';
+const supabaseUrl = 'https://zbimmiyvlupzmklegvtp.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpiaW1taXl2bHVwem1rbGVndnRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIwNzE0MDksImV4cCI6MjA4NzY0NzQwOX0.RcwWg8vD1ZMjE48vYRH81JnnHb0n9QEXfp2-0WGX1Ys';
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Supabase URL and Anon Key must be provided.');
